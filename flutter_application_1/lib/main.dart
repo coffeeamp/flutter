4171407026473146b8f568fce944a14f28091fe7
coffeeamp <coffeeamp@naver.com>
2022-12-21 22:13:01 +0900

    221221

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  
+      home: Image.asset('assets/images/dog.png'),
       // 아이콘위젯 Icon(Icons.home)
-      // 이미지위젯 Image.network('https://picsum.photos/250?image=9'), 
+      // 이미지위젯 Image.network('https://picsum.photos/250?image=9'),
       // Image.asset('경로') // pubspec.yaml에 assets: - images/ 경로를 추가해야함
       // 박스위젯 Container(color: Colors.red, width: 100, height: 100)
       // 텍스트위젯 Text('Hello World', style: TextStyle(fontSize: 40))
-      ); 
+    );
   }
 }
